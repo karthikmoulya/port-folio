c86778f3ecc9a582a6818f400de4becc068b0d01
Karthik Moulya <karthik@pop-os.localdomain>
2020-05-21 14:09:36 +0530

    Updates

---
+++ precache-manifest.f95db7ce9cf7a4281fb9b22d7266ae7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85fd2bd1c65488f6a9a8c1820d63868",
+    "url": "/port-folio/index.html"
+  },
+  {
+    "revision": "55de99dee916ce4f61a5",
+    "url": "/port-folio/static/css/main.5cc07f7e.chunk.css"
+  },
+  {
+    "revision": "bd3819630c6cbaed1b85",
+    "url": "/port-folio/static/js/2.e2785f80.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/port-folio/static/js/2.e2785f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55de99dee916ce4f61a5",
+    "url": "/port-folio/static/js/main.a2697c4e.chunk.js"
+  },
+  {
+    "revision": "cfd6bf8e52bf9e86bb3a",
+    "url": "/port-folio/static/js/runtime-main.d48780da.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/port-folio/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
